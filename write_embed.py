751baa3b845a15f1b6e2ec19c5fcab1e013b9a73
timchen0618 <chen87618@gmail.com>
2025-12-04 16:59:27 -0500

    qampari run in shards

--- write_embed.py
+++ write_embed.py
@@ -1,18 +1,38 @@
 import json
 
-template_file = "embed_template.SBATCH"
-org_template = open(template_file, "r").read()
-write_to_run_file = []
+command = 'gen_embed'
 
-for model in ["infly", "qwen3-0.6b"]:
-    for num in range(0, 16):
-        
-        template = org_template.replace("[model]", model).replace("[num]", str(num))
-        write_to_run_file.append(f"sbatch sbatch_jobs/embed_{model}_{num}.SBATCH")
-        with open(f"sbatch_jobs/embed_{model}_{num}.SBATCH", "w") as f:
-            f.write(template)
+if command == 'gen_embed':
+    template_file = "embed_template.SBATCH"
+    org_template = open(template_file, "r").read()
+    write_to_run_file = []
+
+    # for model in ["infly", "qwen3-0.6b"]:
+    for model in ["contriever"]:
+        for num in range(0, 16):
             
-            
-with open("run_embed.sh", "w") as f:
-    f.write("#!/bin/bash\n")
-    f.write("\n".join(write_to_run_file))
+            template = org_template.replace("[model]", model).replace("[num]", str(num))
+            write_to_run_file.append(f"sbatch sbatch_jobs/embed_{model}_{num}.SBATCH")
+            with open(f"sbatch_jobs/embed_{model}_{num}.SBATCH", "w") as f:
+                f.write(template)
+                
+                
+    with open("run_embed.sh", "w") as f:
+        f.write("#!/bin/bash\n")
+        f.write("\n".join(write_to_run_file))
+
+elif command == 'run_agentic':
+    write_to_run_file = []
+    # for model in ["infly", "qwen3-0.6b"]:
+    for model in ["contriever"]:
+        template_file = f"qampari_{model}.SBATCH"
+        org_template = open(template_file, "r").read()
+        for num in range(0, 10):
+            template = org_template.replace("[num]", str(num))
+            write_to_run_file.append(f"sbatch sbatch_jobs/qampari_{model}_{num}.SBATCH")
+            with open(f"sbatch_jobs/qampari_{model}_{num}.SBATCH", "w") as f:
+                f.write(template)
+                
+    with open("run_qampari.sh", "w") as f:
+        f.write("#!/bin/bash\n")
+        f.write("\n".join(write_to_run_file))
