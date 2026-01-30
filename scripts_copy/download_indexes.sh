a73077c8f3975eac97bb6a17d0c7ef90046302d6
timchen0618 <chen87618@gmail.com>
2026-01-30 17:54:31 -0500

    delete

--- scripts_copy/download_indexes.sh
+++
@@ -1,4 +0,0 @@
-huggingface-cli download Tevatron/browsecomp-plus-indexes --repo-type=dataset --include="bm25/*" --local-dir ./indexes
-huggingface-cli download Tevatron/browsecomp-plus-indexes --repo-type=dataset --include="qwen3-embedding-0.6b/*" --local-dir ./indexes
-huggingface-cli download Tevatron/browsecomp-plus-indexes --repo-type=dataset --include="qwen3-embedding-4b/*" --local-dir ./indexes
-huggingface-cli download Tevatron/browsecomp-plus-indexes --repo-type=dataset --include="qwen3-embedding-8b/*" --local-dir ./indexes
