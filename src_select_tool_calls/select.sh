8eb120fe7888310bf17169b6146ca5ab82d5ed28
timchen0618 <tchen87618@gmail.com>
2026-04-29 11:44:56 -0400

    Final Change to select tool calls before further experiments

--- src_select_tool_calls/select.sh
+++ src_select_tool_calls/select.sh
@@ -3,16 +3,16 @@ REASONING_MAX_CHARS=2000
 TOOL_OUTPUT_MAX_CHARS=3000
 CONTEXT_MAX_CHARS=280000
 
-# gpt-oss-120b (830 files in seed4)
-python src_select_tool_calls/select_useful_tool_calls.py --k 5 --use-original-messages \
-  --preview-chars ${PREVIEW_CHARS} \
-  --context-reasoning-max-chars ${REASONING_MAX_CHARS} \
-  --context-tool-max-chars ${TOOL_OUTPUT_MAX_CHARS} \
-  --context-max-chars ${CONTEXT_MAX_CHARS} \
-  --output selected_tool_calls/all/gpt-oss-120b/seed0/selected_tool_calls_gpt-oss-120b_use_original_messages_gemini_3.1-pro-preview.jsonl \
-  --trajectory-dir runs/bcp/Qwen3-Embedding-8B/full/gpt-oss-120b/seed4/ \
-  --num-threads 8 \
-  --model @vertexai-gemini-ec5413/gemini-3.1-pro-preview
+# # gpt-oss-120b (830 files in seed4)
+# python src_select_tool_calls/select_useful_tool_calls.py --k 5 --use-original-messages \
+#   --preview-chars ${PREVIEW_CHARS} \
+#   --context-reasoning-max-chars ${REASONING_MAX_CHARS} \
+#   --context-tool-max-chars ${TOOL_OUTPUT_MAX_CHARS} \
+#   --context-max-chars ${CONTEXT_MAX_CHARS} \
+#   --output selected_tool_calls/all/gpt-oss-120b/seed0/selected_tool_calls_gpt-oss-120b_use_original_messages_gemini_3.1-pro-preview.jsonl \
+#   --trajectory-dir runs/bcp/Qwen3-Embedding-8B/full/gpt-oss-120b/seed4/ \
+#   --num-threads 8 \
+#   --model @vertexai-gemini-ec5413/gemini-3.1-pro-preview
 
 # # tongyi (830 files in seed4)
 # python src_select_tool_calls/select_useful_tool_calls_tongyi.py --k 5  \
